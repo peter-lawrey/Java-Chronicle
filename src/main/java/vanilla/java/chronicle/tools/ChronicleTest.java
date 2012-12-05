55461a1093418a807f58f296c43f01f2f1278279
Peter Lawrey <peter.lawrey@mlp.com>
2012-12-05 13:38:40 +0000

    Add asString to extract text from an Excerpt for debugging purposes.

--- src/main/java/vanilla/java/chronicle/tools/ChronicleTest.java
+++ src/main/java/vanilla/java/chronicle/tools/ChronicleTest.java
@@ -10,15 +10,33 @@ import java.io.File;
 public enum ChronicleTest {
     ;
 
+    /**
+     * Delete a chronicle on exit, for testing
+     *
+     * @param basePath of the chronicle
+     */
     public static void deleteOnExit(String basePath) {
         new File(basePath + ".data").deleteOnExit();
         new File(basePath + ".index").deleteOnExit();
     }
 
+    /**
+     * Take a text copy of the contents of the Excerpt without changing it's position. Can be called in the debugger.
+     *
+     * @param excerpt to get text from
+     * @return 256 bytes as text with `.` replacing special bytes.
+     */
     public static String asString(Excerpt excerpt) {
         return asString(excerpt, 256);
     }
 
+    /**
+     * Take a text copy of the contents of the Excerpt without changing it's position. Can be called in the debugger.
+     *
+     * @param excerpt to get text from
+     * @param length  the maximum length
+     * @return length bytes as text with `.` replacing special bytes.
+     */
     public static String asString(Excerpt excerpt, int length) {
         int position = excerpt.position();
         int limit = Math.min(position + length, excerpt.length());
