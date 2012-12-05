bec90977f48987717cf53a0900c30f18750c1ef5
Peter Lawrey <peter.lawrey@mlp.com>
2012-12-05 13:36:31 +0000

    Add asString to extract text from an Excerpt for debugging purposes.

--- src/main/java/vanilla/java/chronicle/tools/ChronicleTest.java
+++ src/main/java/vanilla/java/chronicle/tools/ChronicleTest.java
@@ -1,5 +1,7 @@
 package vanilla.java.chronicle.tools;
 
+import vanilla.java.chronicle.Excerpt;
+
 import java.io.File;
 
 /**
@@ -11,5 +13,21 @@ public enum ChronicleTest {
     public static void deleteOnExit(String basePath) {
         new File(basePath + ".data").deleteOnExit();
         new File(basePath + ".index").deleteOnExit();
+    }
+
+    public static String asString(Excerpt excerpt) {
+        return asString(excerpt, 256);
+    }
+
+    public static String asString(Excerpt excerpt, int length) {
+        int position = excerpt.position();
+        int limit = Math.min(position + length, excerpt.length());
+        StringBuilder sb = new StringBuilder(limit - position);
+        for (int i = position; i < limit; i++) {
+            char ch = (char) excerpt.readUnsignedByte(i);
+            if (ch < ' ' || ch > 127) ch = '.';
+            sb.append(ch);
+        }
+        return sb.toString();
     }
 }
