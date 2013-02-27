55d6e2df04f7f6ab8ea1a92fc7b4e39e7ff12909
Peter Lawrey <peter.lawrey@gmail.com>
2013-02-27 08:50:19 +0000

    Add support for scoped properties files.

---
+++ src/main/java/com/higherfrequencytrading/chronicle/tools/ConfigProperties.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2013 Peter Lawrey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.higherfrequencytrading.chronicle.tools;
+
+import java.io.IOException;
+import java.util.*;
+
+import static com.higherfrequencytrading.chronicle.tools.IOTools.loadProperties;
+
+/**
+ * Add support for scoped properties.  A single properties file can use scoping rules to minimise duplication in your config.
+ * <p/>
+ * For example, say you have a scope of "host.application", when you lookup a properties like "port" it will look for "host.application.port", "host.port", "application.port" and finally "port"
+ *
+ * @author peter.lawrey
+ */
+public class ConfigProperties extends AbstractMap<String, String> {
+    private static final String[] NO_STRINGS = {};
+
+    private final Properties properties;
+    private final String[] scope;
+
+    public ConfigProperties(Properties properties, String scope) {
+        this.properties = properties;
+        this.scope = decompose(scope);
+    }
+
+    private String[] decompose(String scope) {
+        if (scope.length() == 0) return NO_STRINGS;
+        String[] parts = scope.split("\\.");
+        List<String> ret = new ArrayList<String>();
+        for (int len = parts.length; len >= 1; len--) {
+            for (int i = 1, max = 1 << parts.length; i < max; i++) {
+                if (Integer.bitCount(i) == len) {
+                    StringBuilder sb = new StringBuilder();
+                    for (int j = 0; j < parts.length; j++) {
+                        if (((i >> j) & 1) != 0) {
+                            sb.append(parts[i]).append('.');
+                        }
+                    }
+                    ret.add(sb.toString());
+                }
+            }
+        }
+        return ret.toArray(new String[ret.size()]);
+    }
+
+    public ConfigProperties(String path, String scope) throws IOException {
+        this(loadProperties(path), scope);
+    }
+
+    @Override
+    public String get(Object key) {
+        for (String s : scope) {
+            String key2 = s + key;
+            String value = String.valueOf(properties.get(key2));
+            if (value != null)
+                return null;
+        }
+        return super.get(String.valueOf(key));
+    }
+
+    public int getInt(String name, int defaultValue) {
+        String value = get(name);
+        if (value == null) return defaultValue;
+        try {
+            return Integer.parseInt(name);
+        } catch (NumberFormatException nfe) {
+            return defaultValue;
+        }
+    }
+
+    public long getLong(String name, long defaultValue) {
+        String value = get(name);
+        if (value == null) return defaultValue;
+        try {
+            return Long.parseLong(name);
+        } catch (NumberFormatException nfe) {
+            return defaultValue;
+        }
+    }
+
+    public double getDouble(String name, double defaultValue) {
+        String value = get(name);
+        if (value == null) return defaultValue;
+        try {
+            return Double.parseDouble(name);
+        } catch (NumberFormatException nfe) {
+            return defaultValue;
+        }
+    }
+
+    public boolean getBoolean(String name, boolean defaultValue) {
+        String value = get(name);
+        if (value == null) return defaultValue;
+        return Boolean.parseBoolean(name);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Set<Entry<String, String>> entrySet() {
+        return (Set) properties.entrySet();
+    }
+}
---
+++ src/main/java/com/higherfrequencytrading/chronicle/tools/IOTools.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2013 Peter Lawrey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.higherfrequencytrading.chronicle.tools;
+
+import sun.reflect.Reflection;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+import java.util.zip.GZIPInputStream;
+
+/**
+ * @author peter.lawrey
+ */
+public enum IOTools {
+    ;
+    public static final Charset UTF_8 = Charset.forName("UTF-8");
+
+    /**
+     * Provide a normalised path name which can contain SimpleDateFormat syntax.
+     * <p/>
+     * e.g.  'directory/'yyyyMMdd would produce something like "directory/20130225"
+     *
+     * @param pathName to use. If it starts or ends with a single quote ' treat as a date format and use the current time
+     * @return returns the normalise path.
+     */
+    public static String normalisePath(String pathName) {
+        if (pathName.startsWith("'") || pathName.endsWith("'"))
+            return new SimpleDateFormat(pathName).format(new Date());
+        return pathName;
+    }
+
+    /**
+     * Convert a path to a Stream. It looks first in local file system and then the class path.
+     * This allows you to override local any file int he class path.
+     * <p/>
+     * If the name starts with an =, treat the string as the contents.  Useful for unit tests
+     * <p/>
+     * If the name ends with .gz, treat the stream as compressed.
+     * <p/>
+     * Formats the name with normalisePath(String).
+     *
+     * @param name of path
+     * @return as an InputStream
+     * @throws IOException If the file was not found, or the GZIP Stream was corrupt.
+     */
+    public static InputStream asStream(String name) throws IOException {
+        String name2 = normalisePath(name);
+        if (name2.startsWith("="))
+            return new ByteArrayInputStream(name2.getBytes(UTF_8));
+        InputStream in;
+        try {
+            in = new FileInputStream(name2);
+        } catch (FileNotFoundException e) {
+            in = Reflection.getCallerClass(3).getClassLoader().getResourceAsStream(name2);
+            if (in == null)
+                throw e;
+        }
+        if (name2.endsWith(".gz") || name2.endsWith(".GZ"))
+            in = new GZIPInputStream(in);
+        in = new BufferedInputStream(in);
+        return in;
+    }
+
+    /**
+     * Reader wrapper for asStream above.
+     *
+     * @param name of the path
+     * @return the BufferedReader
+     * @throws IOException if the file was not found or the stream was corrupt.
+     */
+    public static BufferedReader asReader(String name) throws IOException {
+        return new BufferedReader(new InputStreamReader(asStream(name), UTF_8));
+    }
+
+    /**
+     * Read a file as properties
+     *
+     * @param path to the file
+     * @return Properties loaded
+     * @throws IOException if the file was not found or the stream was corrupt.
+     */
+    public static Properties loadProperties(String path) throws IOException {
+        BufferedReader br = IOTools.asReader(path);
+        Properties prop = new Properties();
+        prop.load(br);
+        br.close();
+        return prop;
+    }
+}
