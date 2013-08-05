f4221540ff8cf848004b04f52ea5aa45aacd6429
Peter Lawrey <peter.lawrey@gmail.com>
2013-08-05 20:20:56 +0100

    demo2

--- demo2/src/main/java/vanilla/java/stages/SinkMain.java
+++ demo2/src/main/java/vanilla/java/stages/SinkMain.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Peter Lawrey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package vanilla.java.stages;
 
 import com.higherfrequencytrading.chronicle.Chronicle;
@@ -73,7 +89,7 @@ public class SinkMain {
             count++;
 
             if (count == MESSAGES) {
-                System.out.printf("latencies\t50%%\t90%%\t99%%\t99.9%%\t99.99%%n");
+                System.out.printf("latencies\t50%%\t90%%\t99%%\t99.9%%\t99.99%%%n");
                 for (int i = 0; i < timings.length; i++) {
                     Arrays.sort(timings[i]);
                     long t0 = timings[i][0];
@@ -93,7 +109,7 @@ public class SinkMain {
                             t99_99 += correction;
                             break;
                     }
-                    System.out.printf("%s-%s\t%s\t%s\t%s\t%s\t%sus%n",
+                    System.out.printf("%s-%s\t%s\t%s\t%s\t%s\t%s us%n",
                             VALUES[i].name(), VALUES[i + 1].name(),
                             t50 / 1e3,
                             t90 / 1e3,
