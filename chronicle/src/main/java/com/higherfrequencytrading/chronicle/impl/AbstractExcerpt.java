0561a7cf962fe7dd8395af7852a98fb372c35788
Peter Lawrey <peter.lawrey@gmail.com>
2013-10-11 15:03:20 +0100

    Add range searches

--- chronicle/src/main/java/com/higherfrequencytrading/chronicle/impl/AbstractExcerpt.java
+++ chronicle/src/main/java/com/higherfrequencytrading/chronicle/impl/AbstractExcerpt.java
@@ -1997,6 +1997,81 @@ public abstract class AbstractExcerpt implements Excerpt {
         return this.size = size;
     }
 
+
+    @Override
+    public long findExact(ExcerptComparator comparator) {
+        long lo = 0, hi = size() - 1;
+        while (lo <= hi) {
+            long mid = (hi + lo) >>> 1;
+            boolean found = index(mid);
+            assert found;
+
+            int cmp = comparator.compare(this);
+            finish();
+            if (cmp < 0)
+                lo = mid + 1;
+            else if (cmp > 0)
+                hi = mid - 1;
+            else
+                return mid; // key found
+        }
+        return ~lo; // -(lo + 1)
+    }
+
+    @Override
+    public void findRange(long[] startEnd, ExcerptComparator comparator) {
+        // lower search range
+        long lo1 = 0, hi1 = size() - 1;
+        // upper search range
+        long lo2 = 0, hi2 = hi1;
+        boolean both = true;
+        // search for the low values.
+        while (lo1 <= hi1) {
+            long mid = (hi1 + lo1) >>> 1;
+            boolean found = index(mid);
+            assert found;
+
+            int cmp = comparator.compare(this);
+            finish();
+
+            if (cmp < 0) {
+                lo1 = mid + 1;
+                if (both)
+                    lo2 = lo1;
+            } else if (cmp > 0) {
+                hi1 = mid - 1;
+                if (both)
+                    hi2 = hi1;
+            } else {
+                hi1 = mid - 1;
+                if (both)
+                    lo2 = mid + 1;
+                both = false;
+            }
+        }
+        // search for the high values.
+        while (lo2 <= hi2) {
+            long mid = (hi2 + lo2) >>> 1;
+            if (!index(mid)) {
+                if (mid > lo2)
+                    index(--mid);
+                else
+                    break;
+            }
+            int cmp = comparator.compare(this);
+            finish();
+
+            if (cmp <= 0) {
+                lo2 = mid + 1;
+            } else {
+                hi2 = mid - 1;
+            }
+        }
+        startEnd[0] = lo1; // inclusive
+        startEnd[1] = lo2; // exclusive
+    }
+
+
     protected class ExcerptInputStream extends InputStream {
         private int mark = 0;
 
--- chronicle/src/test/java/com/higherfrequencytrading/db/DataStorePerfTest.java
+++ chronicle/src/test/java/com/higherfrequencytrading/db/DataStorePerfTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Peter Lawrey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.higherfrequencytrading.db;
 
 import com.higherfrequencytrading.chronicle.datamodel.DataStore;
@@ -18,69 +34,69 @@ import java.util.Random;
  * Time: 15:59
  */
 public class DataStorePerfTest {
-	private static final String TMP = System.getProperty("java.io.tmpdir");
+    private static final String TMP = System.getProperty("java.io.tmpdir");
 
-	public static void main(String[] args) throws IOException {
-		String basePath = TMP + "/DataStorePerfTest";
-		ChronicleTools.deleteOnExit(basePath);
-		IndexedChronicle chronicle = new IndexedChronicle(basePath);
-		chronicle.useUnsafe(true);
-		DataStore dataStore = new DataStore(chronicle, ModelMode.MASTER);
-		MapWrapper<String, byte[]> map = new MapWrapper<String, byte[]>(dataStore, "map",
-				String.class, byte[].class, new LinkedHashMap<String, byte[]>(), 160);
-		dataStore.add("map", map);
-		dataStore.start();
+    public static void main(String[] args) throws IOException {
+        String basePath = TMP + "/DataStorePerfTest";
+        ChronicleTools.deleteOnExit(basePath);
+        IndexedChronicle chronicle = new IndexedChronicle(basePath);
+        chronicle.useUnsafe(true);
+        DataStore dataStore = new DataStore(chronicle, ModelMode.MASTER);
+        MapWrapper<String, byte[]> map = new MapWrapper<String, byte[]>(dataStore, "map",
+                String.class, byte[].class, new LinkedHashMap<String, byte[]>(), 160);
+        dataStore.add("map", map);
+        dataStore.start();
 
-		// generate keys and values
-		int keyCount = 100000;
-		String[] keys = new String[keyCount];
-		byte[][] bytes = new byte[keyCount][];
-		Random rand = new Random();
-		for (int i = 0; i < keyCount; i++) {
-			StringBuilder key = new StringBuilder();
-			do {
-				key.append(Integer.toString(rand.nextInt() & Integer.MAX_VALUE, 36));
-			} while (key.length() < 16);
-			key.setLength(16);
-			keys[i] = key.toString();
-			bytes[i] = new byte[100];
-		}
-		Collections.sort(Arrays.asList(keys));
+        // generate keys and values
+        int keyCount = 100000;
+        String[] keys = new String[keyCount];
+        byte[][] bytes = new byte[keyCount][];
+        Random rand = new Random();
+        for (int i = 0; i < keyCount; i++) {
+            StringBuilder key = new StringBuilder();
+            do {
+                key.append(Integer.toString(rand.nextInt() & Integer.MAX_VALUE, 36));
+            } while (key.length() < 16);
+            key.setLength(16);
+            keys[i] = key.toString();
+            bytes[i] = new byte[100];
+        }
+        Collections.sort(Arrays.asList(keys));
 
-		for (int t = 0; t < 10; t++) {
-			map.clear();
+        for (int t = 0; t < 10; t++) {
+            map.clear();
 
-			long time0 = System.nanoTime();
-			// Start with sequential writes
-			for (int i = 0; i < keyCount; i++) {
-				map.put(keys[i], bytes[i]);
-			}
-			long time1 = System.nanoTime();
-			// Sequential reads
-			for (int i = 0; i < keyCount; i++) {
-				byte[] bytes0 = map.get(keys[i]);
-			}
-			long time2 = System.nanoTime();
-			String[] keys2 = keys.clone();
-			Collections.shuffle(Arrays.asList(keys2));
-			long time3 = System.nanoTime();
-			// random writes
-			for (int i = 0; i < keyCount; i++) {
-				map.put(keys[i], bytes[i]);
-			}
-			long time4 = System.nanoTime();
-			// random reads
-			for (int i = 0; i < keyCount; i++) {
-				byte[] bytes0 = map.get(keys[i]);
-			}
-			long time5 = System.nanoTime();
-			System.out.printf("Seq write: %,d K/s, Seq read: %,d K/s, Rnd write: %,d K/s, Rnd read: %,d K/s%n",
-					keyCount * 1000000L / (time1 - time0),
-					keyCount * 1000000L / (time2 - time1),
-					keyCount * 1000000L / (time4 - time3),
-					keyCount * 1000000L / (time5 - time4)
-			);
-		}
-		chronicle.close();
-	}
+            long time0 = System.nanoTime();
+            // Start with sequential writes
+            for (int i = 0; i < keyCount; i++) {
+                map.put(keys[i], bytes[i]);
+            }
+            long time1 = System.nanoTime();
+            // Sequential reads
+            for (int i = 0; i < keyCount; i++) {
+                byte[] bytes0 = map.get(keys[i]);
+            }
+            long time2 = System.nanoTime();
+            String[] keys2 = keys.clone();
+            Collections.shuffle(Arrays.asList(keys2));
+            long time3 = System.nanoTime();
+            // random writes
+            for (int i = 0; i < keyCount; i++) {
+                map.put(keys[i], bytes[i]);
+            }
+            long time4 = System.nanoTime();
+            // random reads
+            for (int i = 0; i < keyCount; i++) {
+                byte[] bytes0 = map.get(keys[i]);
+            }
+            long time5 = System.nanoTime();
+            System.out.printf("Seq write: %,d K/s, Seq read: %,d K/s, Rnd write: %,d K/s, Rnd read: %,d K/s%n",
+                    keyCount * 1000000L / (time1 - time0),
+                    keyCount * 1000000L / (time2 - time1),
+                    keyCount * 1000000L / (time4 - time3),
+                    keyCount * 1000000L / (time5 - time4)
+            );
+        }
+        chronicle.close();
+    }
 }
